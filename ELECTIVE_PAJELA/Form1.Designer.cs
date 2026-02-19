ffae34c9b124f5b283132e76f452b825368be935
Miguel Pajela <Miguel Pajela@LAPTOP-RF7MTOVT>
2026-02-19 15:07:05 +0800

    elective

--- ELECTIVE_PAJELA/Form1.Designer.cs
+++ ELECTIVE_PAJELA/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label5 = new System.Windows.Forms.Label();
@@ -39,9 +40,13 @@
             this.radioButton6 = new System.Windows.Forms.RadioButton();
             this.label7 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtProductQuantity = new System.Windows.Forms.TextBox();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -70,18 +75,12 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label11 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -100,13 +99,12 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(560, 270);
             this.dataGridView1.TabIndex = 7;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(439, 298);
+            this.label5.Location = new System.Drawing.Point(429, 240);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(234, 25);
             this.label5.TabIndex = 10;
@@ -116,7 +114,7 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(454, 333);
+            this.radioButton1.Location = new System.Drawing.Point(444, 275);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(66, 20);
             this.radioButton1.TabIndex = 11;
@@ -128,7 +126,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(454, 356);
+            this.radioButton2.Location = new System.Drawing.Point(444, 298);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(65, 20);
             this.radioButton2.TabIndex = 12;
@@ -140,7 +138,7 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(454, 379);
+            this.radioButton3.Location = new System.Drawing.Point(444, 321);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(112, 20);
             this.radioButton3.TabIndex = 13;
@@ -152,7 +150,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(456, 422);
+            this.label6.Location = new System.Drawing.Point(446, 364);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(128, 25);
             this.label6.TabIndex = 15;
@@ -162,7 +160,7 @@
             // 
             this.radioButton5.AutoSize = true;
             this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton5.Location = new System.Drawing.Point(461, 467);
+            this.radioButton5.Location = new System.Drawing.Point(451, 409);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(82, 20);
             this.radioButton5.TabIndex = 16;
@@ -174,7 +172,7 @@
             // 
             this.radioButton6.AutoSize = true;
             this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton6.Location = new System.Drawing.Point(461, 507);
+            this.radioButton6.Location = new System.Drawing.Point(451, 449);
             this.radioButton6.Name = "radioButton6";
             this.radioButton6.Size = new System.Drawing.Size(60, 20);
             this.radioButton6.TabIndex = 17;
@@ -186,20 +184,21 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(869, 209);
+            this.label7.Location = new System.Drawing.Point(838, 209);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(332, 25);
             this.label7.TabIndex = 19;
             this.label7.Text = "PAYMENT/ORDER SUMMARY";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.textBox8);
             this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Controls.Add(this.textBox7);
-            this.groupBox1.Controls.Add(this.textBox6);
-            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.txtProductQuantity);
+            this.groupBox1.Controls.Add(this.txtProductPrice);
+            this.groupBox1.Controls.Add(this.txtProductName);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
@@ -208,31 +207,69 @@
             this.groupBox1.TabIndex = 28;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // textBox7
+            // label12
             // 
-            this.textBox7.Location = new System.Drawing.Point(150, 335);
-            this.textBox7.Multiline = true;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(174, 43);
-            this.textBox7.TabIndex = 11;
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(6, 407);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(128, 24);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "PRODUCT ID:";
             // 
-            // textBox6
+            // textBox8
             // 
-            this.textBox6.Location = new System.Drawing.Point(150, 286);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(174, 43);
-            this.textBox6.TabIndex = 10;
+            this.textBox8.Location = new System.Drawing.Point(150, 397);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(174, 43);
+            this.textBox8.TabIndex = 14;
+            this.textBox8.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox8_KeyDown);
             // 
-            // textBox5
+            // label10
             // 
-            this.textBox5.Location = new System.Drawing.Point(150, 238);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(174, 43);
-            this.textBox5.TabIndex = 9;
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(23, 335);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 24);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "QUANTITY:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(23, 286);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 24);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "PRICE:";
+            // 
+            // txtProductQuantity
+            // 
+            this.txtProductQuantity.Location = new System.Drawing.Point(150, 335);
+            this.txtProductQuantity.Multiline = true;
+            this.txtProductQuantity.Name = "txtProductQuantity";
+            this.txtProductQuantity.Size = new System.Drawing.Size(174, 43);
+            this.txtProductQuantity.TabIndex = 11;
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Location = new System.Drawing.Point(150, 286);
+            this.txtProductPrice.Multiline = true;
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(174, 43);
+            this.txtProductPrice.TabIndex = 10;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(150, 238);
+            this.txtProductName.Multiline = true;
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(174, 43);
+            this.txtProductName.TabIndex = 9;
             // 
             // label1
             // 
@@ -274,7 +311,6 @@
             this.groupBox2.TabIndex = 29;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
-            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
             // label3
             // 
@@ -507,7 +543,7 @@
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox2.Image = global::ELECTIVE_PAJELA.Properties.Resources._78260956_5fb2_45b5_b857_2d098288fe3c;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(397, 19);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(157, 122);
@@ -515,61 +551,11 @@
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox3.Location = new System.Drawing.Point(432, 31);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(190, 160);
-            this.pictureBox3.TabIndex = 12;
-            this.pictureBox3.TabStop = false;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(23, 286);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(70, 24);
-            this.label9.TabIndex = 12;
-            this.label9.Text = "PRICE:";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(23, 335);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(110, 24);
-            this.label10.TabIndex = 13;
-            this.label10.Text = "QUANTITY:";
-            // 
-            // button16
-            // 
-            this.button16.Location = new System.Drawing.Point(411, 209);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(106, 41);
-            this.button16.TabIndex = 72;
-            this.button16.Text = "SCAN";
-            this.button16.UseVisualStyleBackColor = true;
-            // 
-            // button17
-            // 
-            this.button17.Location = new System.Drawing.Point(552, 209);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(106, 41);
-            this.button17.TabIndex = 73;
-            this.button17.Text = "STOP";
-            this.button17.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1312, 836);
-            this.Controls.Add(this.button17);
-            this.Controls.Add(this.button16);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button3);
@@ -585,7 +571,6 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
@@ -595,7 +580,6 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -638,17 +622,16 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtProductQuantity;
+        private System.Windows.Forms.TextBox txtProductName;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtProductPrice;
     }
 }
 
--- ELECTIVE_PAJELA/Form1.cs
+++ ELECTIVE_PAJELA/Form1.cs
@@ -1,45 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ELECTIVE_PAJELA
 {
     public partial class Form1 : Form
     {
+        private string _connectionString = @"Data Source=LAPTOP-RF7MTOVT\SQLEXPRESS;Initial Catalog=InventoryDB;Integrated Security=True;TrustServerCertificate=True";
+        private DataTable scannedProductsTable;
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.AutoGenerateColumns = true;
+            InitializeScannedProductsTable();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        // Initialize the DataTable to hold scanned products (temporary, not saved to DB)
+        private void InitializeScannedProductsTable()
         {
+            scannedProductsTable = new DataTable();
+            scannedProductsTable.Columns.Add("Product_Name", typeof(string));
+            scannedProductsTable.Columns.Add("Product_Price", typeof(decimal));
+            scannedProductsTable.Columns.Add("Stock_Quantity", typeof(int));
+            scannedProductsTable.Columns.Add("Scan_Count", typeof(int)); // Track how many times scanned
 
+            dataGridView1.DataSource = scannedProductsTable.DefaultView;
         }
 
-        private void label7_Click(object sender, EventArgs e)
+        private void textBox8_KeyDown(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrWhiteSpace(textBox8.Text))
+            {
+                string barcode = textBox8.Text.Trim();
+                AddOrIncrementProductByBarcode(barcode);
+                textBox8.Clear();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void AddOrIncrementProductByBarcode(string barcode)
         {
+            // Check if barcode already exists in the DataTable
+            DataRow[] foundRows = scannedProductsTable.Select($"Barcode = '{barcode.Replace("'", "''")}'");
+            if (foundRows.Length > 0)
+            {
+                // Increment the scan count
+                foundRows[0]["Scan_Count"] = Convert.ToInt32(foundRows[0]["Scan_Count"]) + 1;
+                scannedProductsTable.AcceptChanges();
 
-        }
+                // Update textboxes with existing row data
+                txtProductName.Text = foundRows[0]["Product_Name"].ToString();
+                txtProductPrice.Text = foundRows[0]["Product_Price"].ToString();
+                txtProductQuantity.Text = foundRows[0]["Stock_Quantity"].ToString();
+            }
+            else
+            {
+                // Load product from database and add to DataTable (temporary, not saved to DB)
+                try
+                {
+                    using (SqlConnection cn = new SqlConnection(_connectionString))
+                    {
+                        cn.Open();
+                        string sql = @"SELECT Product_Name, Product_Price, Stock_Quantity, Barcode
+                                       FROM PRODUCTS
+                                       WHERE Barcode = @barcode";
+                        using (SqlCommand cmd = new SqlCommand(sql, cn))
+                        {
+                            cmd.Parameters.AddWithValue("@barcode", barcode);
+                            using (SqlDataReader dr = cmd.ExecuteReader())
+                            {
+                                if (dr.Read())
+                                {
+                                    DataRow newRow = scannedProductsTable.NewRow();
+                                    newRow["Product_Name"] = dr["Product_Name"];
+                                    newRow["Product_Price"] = dr["Product_Price"];
+                                    newRow["Stock_Quantity"] = dr["Stock_Quantity"];
+                                    newRow["Barcode"] = dr["Barcode"];
+                                    newRow["Scan_Count"] = 1;
+                                    scannedProductsTable.Rows.Add(newRow);
+                                    scannedProductsTable.AcceptChanges();
 
-        private void groupBox2_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
+                                    // Update textboxes with new data
+                                    txtProductName.Text = dr["Product_Name"].ToString();
+                                    txtProductPrice.Text = dr["Product_Price"].ToString();
+                                    txtProductQuantity.Text = dr["Stock_Quantity"].ToString();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Product not found for barcode: " + barcode, "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    txtProductName.Clear();
+                                    txtProductPrice.Clear();
+                                    txtProductQuantity.Clear();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading product: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtProductName.Clear();
+                    txtProductPrice.Clear();
+                    txtProductQuantity.Clear();
+                }
+            }
         }
     }
 }
